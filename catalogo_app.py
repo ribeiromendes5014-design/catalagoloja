658ebd40f5bc2d6bc0fa1d23aa81ecdedb00cb63
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-11 23:19:28 -0300

    Update catalogo_app.py

--- catalogo_app.py
+++ catalogo_app.py
@@ -93,6 +93,8 @@ div[data-testid="stPopover"] > div:first-child > button {{
     opacity: 0 !important;
     pointer-events: auto !important;
 }}
+</style>
+""", unsafe_allow_html=True)
 
 .stApp {{ background-image: url({BACKGROUND_IMAGE_URL}) !important; background-size: cover; background-attachment: fixed; }}
 
@@ -444,3 +446,4 @@ whatsapp_button_html = f"""
 </a>
 """
 st.markdown(whatsapp_button_html, unsafe_allow_html=True)
+
