0d4a60c728f2d4b9c7c1cd3b0883b1d9da76fe88
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-16 19:07:45 -0300

    Update detalhes_produto_ui.py

--- detalhes_produto_ui.py
+++ detalhes_produto_ui.py
@@ -389,7 +389,7 @@ def mostrar_detalhes_produto(df_catalogo_indexado):
                 
                 qtd = st.session_state.get(key_qtd, 1)
                 
-                adicionar_qtd_ao_carrinho(row_produto_selecionado, qtd, preco_final_selecionado)
+                adicionar_qtd_ao_carrinho(id_produto_selecionado, row_produto_selecionado, qtd)
                 
                 st.toast(f"{qtd}x {row_produto_selecionado['NOME']} adicionado(s)!")
                 time.sleep(0.5)
@@ -468,5 +468,6 @@ def mostrar_detalhes_produto(df_catalogo_indexado):
                         st.write("<br>", unsafe_allow_html=True) 
 
                     st.write(f"<h5 style='color: #880E4F; margin:0;'>R$ {preco_card_final:,.2f}</h5>", unsafe_allow_html=True)
+
 
 
